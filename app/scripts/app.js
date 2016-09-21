0570099032a30c2ce0f3d3112e9a6a78eaa952a3
sid2290 <sudhanshunarang90@gmail.com>
2016-09-21 15:35:02 -0600

    Create SPA issue #5

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,27 @@
 "use strict";
 
-angular.module('confusionApp',[]);
+angular.module('confusionApp',['ngRoute'])
+    .config(function($routeProvider) {
+        $routeProvider
+            // route for the Home Page
+            .when('/', {
+                templateUrl : 'home.html',
+                controller  : 'IndexController'
+            })
+            // route for the contactus page
+            .when('/contactus', {
+                templateUrl : 'contactus.html',
+                controller  : 'ContactController'
+            })
+            // route for the menu page
+            .when('/menu', {
+                templateUrl : 'menu.html',
+                controller  : 'MenuController'
+            })
+            .when('/aboutus', {
+                templateUrl : 'aboutus.html',
+                controller  : 'AboutController'
+            })
+            .otherwise('/contactus');
+    })
             
