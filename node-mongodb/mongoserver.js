90b1bc581e78811264279ab9e46eacce6571af62
sid2290 <sudhanshunarang90@gmail.com>
2016-09-25 17:40:28 -0600

    Develop a Node module containing some common MongoDB operations (operations.js) Use the Node module in your application and communicate with the MongoDB server (mongoserver.js)

--- node-mongodb/mongoserver.js
+++ node-mongodb/mongoserver.js
@@ -1,31 +1,38 @@
 var MongoClient = require('mongodb').MongoClient,
     assert = require('assert');
 
+var dboper = require('./operations');
+
 // Connection URL
 var url = 'mongodb://localhost:27017/conFusion';
+
 // Use connect method to connect to the Server
 MongoClient.connect(url, function (err, db) {
-        assert.equal(err,null);
-        console.log("Connected correctly to server");
-        
-        var collection = db.collection("dishes");
-        collection.insertOne({name: "Uthapizza", description: "test"},
-        
-        function(err,result) {
-            
-            assert.equal(err,null);
-            console.log("After Insert:");
+    assert.equal(null, err);
+    console.log("Connected correctly to server");
+
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test" },
+        "dishes", function (result) {
             console.log(result.ops);
-            
-        collection.find({}).toArray(function(err,docs) {
-            assert.equal(err,null);
-            console.log("Found:");
-            console.log(docs);
-            
-            db.dropCollection("dishes", function(err, result) {
-               assert.equal(err,null);
-               db.close();
+
+            dboper.findDocuments(db, "dishes", function (docs) {
+                console.log(docs);
+
+                dboper.updateDocument(db, { name: "Vadonut" },
+                    { description: "Updated Test" },
+                    "dishes", function (result) {
+                        console.log(result.result);
+
+                        dboper.findDocuments(db, "dishes", function (docs) {
+                            console.log(docs)
+
+                            db.dropCollection("dishes", function (result) {
+                                console.log(result);
+
+                                db.close();
+                            });
+                        });
+                    });
             });
         });
-    });
 });
