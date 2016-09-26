abe45941e703b4d93d949371543167b9c82de776
sid2290 <sudhanshunarang90@gmail.com>
2016-09-26 16:51:48 -0600

    using create function to create new dish and using findByIdAndUpdate to query and update dish

--- node-mongoose/mongoose-server.js
+++ node-mongoose/mongoose-server.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose'),
     assert = require('assert');
 
 var Dishes = require('./models/dishes-1');
-
 // Connection URL
 var url = 'mongodb://localhost:27017/conFusion';
 mongoose.connect(url);
@@ -11,27 +10,35 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
     // we're connected!
     console.log("Connected correctly to server");
-
-    // create a new user
-    var newDish = Dishes({
+    // create a new dish
+    Dishes.create({
         name: 'Uthapizza',
         description: 'Test'
-    });
-
-    // save the user
-    newDish.save(function (err) {
+    }, function (err, dish) {
         if (err) throw err;
         console.log('Dish created!');
+        console.log(dish);
 
-        // get all the users
-        Dishes.find({}, function (err, dishes) {
-            if (err) throw err;
+        var id = dish._id;
 
-            // object of all the users
-            console.log(dishes);
-                        db.collection('dishes').drop(function () {
-                db.close();
-            });
-        });
+        // get all the dishes
+        setTimeout(function () {
+            Dishes.findByIdAndUpdate(id, {
+                    $set: {
+                        description: 'Updated Test'
+                    }
+                }, {
+                    new: true
+                })
+                .exec(function (err, dish) {
+                    if (err) throw err;
+                    console.log('Updated Dish!');
+                    console.log(dish);
+
+                    db.collection('dishes').drop(function () {
+                        db.close();
+                    });
+                });
+        }, 3000);
     });
 });
